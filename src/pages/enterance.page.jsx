4330b2013e8452deab0c7b398b3f341fa824b5af
abasAlmaayofi <abasalmaayofi@gmail.com>
2024-02-20 16:45:17 +0300

    feat. to be sent 2

--- src/pages/enterance.page.jsx
+++ src/pages/enterance.page.jsx
@@ -5,6 +5,9 @@ import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 import logo from "../assets/logoTransparent.png";
 import clockTower from "../assets/clockTower.png";
+import { RiTwitterLine } from "react-icons/ri";
+import { LuInstagram } from "react-icons/lu";
+import { FaSnapchat } from "react-icons/fa";
 const Entrance = () => {
   return (
     <PageTransitionLayout>
@@ -14,7 +17,7 @@ const Entrance = () => {
           alt="entrance-background"
           className="absolute top-0 w-scree h-screen"
         />
-        <div className="absolute top-0 z-10 flex bg-gray-200/50 flex-col items-center w-screen h-screen">
+        <div className="absolute top-0 z-10 flex bg-gray-200/70 flex-col items-center w-screen h-screen">
           <div>
             <img src={logo} alt="logo" className="w-60 h-60 relative -top-2" />
           </div>
@@ -30,16 +33,45 @@ const Entrance = () => {
             animate={{ y: 0 }}
             transition={{
               type: "spring",
-              duration: 1,
+              duration: 1.5,
               stifness: 150,
             }}
+            className="relative top-10"
           >
-            <Link to="/home" className="">
-              <p className="text-[#3384c3] shadow-md shadow-gray-500/20 w-fit mx-auto h-4 md:text-5xl md:mb-10 font-TrikaRegular text-[25px] bg-[#f2cc3f]  md:text-right text-center leading-relaxed mb-6 bg-opacity-70">
-                <span className="relative -top-5">دخول</span>
+            <Link to="/home">
+              <p className="text-[#3384c3] shadow-md shadow-[#f2cc3f]/50 w-fit mx-auto h-6 md:text-5xl md:mb-10 font-TrikaRegular text-[32px] bg-[#f2cc3f]  md:text-right text-center leading-relaxed mb-6 bg-opacity-70">
+                <span className="relative -top-6">دخول</span>
               </p>
             </Link>
           </motion.div>
+          <div className="relative top-20">
+            <div className="w-full flex my-4 gap-6 justify-center">
+              <Link
+                to="https://twitter.com/lsa_ku?lang=en"
+                className="p-3 border border-[#3384c3]/80 shadow-md shadow-[#f2cc3f]/50 bg-gray-300/10 text-[#3384c3] rounded-lg"
+              >
+                <RiTwitterLine size={16} />
+              </Link>
+              <Link
+                to="https://www.instagram.com/lsa_ku/?hl=en"
+                className="p-3 border border-[#3384c3]/80 shadow-md shadow-[#f2cc3f]/50 bg-gray-300/10 text-[#3384c3] rounded-lg"
+              >
+                <LuInstagram size={16} />
+              </Link>
+              <Link
+                to="https://www.snapchat.com/add/lsa_ku"
+                className="p-3 border border-[#3384c3]/80 shadow-md shadow-[#f2cc3f]/50 bg-gray-300/10 text-[#3384c3] rounded-lg"
+              >
+                <FaSnapchat size={16} />
+              </Link>
+            </div>
+          </div>
+          <div className="relative top-32">
+            <p className="text-sm text-[#3384c3]/70 text-center">
+              {" "}
+              LSA_KU &copy; جميع الحقوق محفوظة لجمعية طلبة كلية الحقوق
+            </p>
+          </div>
         </div>
       </div>
     </PageTransitionLayout>
