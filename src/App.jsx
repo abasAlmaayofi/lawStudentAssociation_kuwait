04e9f53ba453d728b8742538e63e07fa2be490b9
abasAlmaayofi <abasalmaayofi@gmail.com>
2024-02-22 16:13:15 +0300

    feat. finish year three and fourth

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import Contact from "./pages/contact.page";
 import Entrance from "./pages/enterance.page";
 import FirstYear from "./pages/first.page";
 import SecondYear from "./pages/second.page";
+import ThirdYear from "./pages/third.page";
+import FourthYear from "./pages/fourth.page";
+import Faculty from "./pages/faculty.page";
 
 function App() {
   return (
@@ -15,8 +18,9 @@ function App() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/first-year" element={<FirstYear />} />
           <Route path="/second-year" element={<SecondYear />} />
-          <Route path="/third-year" element={<FirstYear />} />
-          <Route path="/fourth-year" element={<FirstYear />} />
+          <Route path="/third-year" element={<ThirdYear />} />
+          <Route path="/fourth-year" element={<FourthYear />} />
+          <Route path="/faculty-accounts" element={<Faculty />} />
           <Route path="/" element={<Entrance />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/faculty.page.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Background from "../assets/background.jpg";
+import File from "@/components/file.component";
+import Header from "@/components/header.component";
+import Footer from "@/components/footer.component";
+import { LuDiamond } from "react-icons/lu";
+import PageTransitionLayout from "@/components/pageTransitionLayout.component";
+import { RiNumber4 } from "react-icons/ri";
+
+const Faculty = () => {
+  return (
+    <PageTransitionLayout>
+      <div className="relative w-screen h-auto">
+        <img
+          src={Background}
+          alt="kuwait-university-collage-of-law"
+          className="w-full md:h-[1220px] h-[1860px] object-fit"
+        />
+        <div className="absolute top-0 w-screen h-full bg-gray-200/50  ">
+          <div className="border-b-4 border-double border-[#3384c3]/90 pb-12">
+            <Header />
+            <p className="text-[#3384c3] mt-12  w-fit mx-auto h-4 md:text-5xl font-vexa text-[25px] bg-[#f2cc3f]  md:text-right text-center leading-relaxed mb-6 bg-opacity-70">
+              <span className="relative -top-5">
+                {" "}
+                أقسام كلية الحقوق و أعضاء
+              </span>
+              {/* <span className="w-full flex justify-center relative -top-2">
+                <RiNumber4
+                  size={40}
+                  className="border-4 border-dashed border-[#f2cc3f]/70 rounded-full p-1 "
+                />
+              </span> */}
+            </p>
+            <p className="text-[#3384c3] mt-2  w-fit mx-auto h-4 md:text-5xl font-vexa text-[25px] bg-[#f2cc3f]  md:text-right text-center leading-relaxed mb-6 bg-opacity-70">
+              <span className="relative -top-5"> هيئة التدريس</span>
+              {/* <span className="w-full flex justify-center relative -top-2">
+                <RiNumber4
+                  size={40}
+                  className="border-4 border-dashed border-[#f2cc3f]/70 rounded-full p-1 "
+                />
+              </span> */}
+            </p>
+          </div>
+          <p className="mt-4  pr-8 font-vexa text-lg tracking-wide text-[#3384c3] flex justify-end items-center gap-2">
+            <span>:إختر إحدى الكليات التالية</span> <LuDiamond />
+          </p>
+
+          <div className="grid grid-cols-2 mt-2 pt-8 md:grid-cols-5 gap-y-12"></div>
+          <Footer />
+        </div>
+      </div>
+    </PageTransitionLayout>
+  );
+};
+
+export default Faculty;
